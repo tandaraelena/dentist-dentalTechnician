19a950d09663acbb8dc5f9559fc1faea306cd936
tandaraelena <tandaraelena@yahoo.com>
2019-06-21 14:45:25 +0100

    style the navbar

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -3,30 +3,25 @@ import "./navbar.scss";
 // import logo from "/images/Untitled.png";
 
 export default class Navbar extends Component {
+  state = {
+    navLink: ["New Patient", "Patients List", "Welcome, MAria!"]
+  };
   render() {
     return (
       <nav className="navbar">
-        <div className="left-side-navbar">
+        <div className="left-column">
           <img src="/images/Untitled.png" alt="logo" className="logo" />
-          <a href="/" className="nav-link">
+          <button href="/" className="chat-link">
             Chat
-          </a>
+          </button>
         </div>
-        <ul className="right-side-navbar">
+        <ul className="right-column">
           <li>
-            <a href="/" className="nav-link">
-              New Patient
-            </a>
-          </li>
-          <li>
-            <a href="/" className="nav-link">
-              Patients List
-            </a>
-          </li>
-          <li>
-            <a href="/" className="nav-link">
-              Welcome, (userName)!
-            </a>
+            {this.state.navLink.map(link => (
+              <button href="/" className="nav-link" key={link}>
+                {link}
+              </button>
+            ))}
           </li>
         </ul>
       </nav>
